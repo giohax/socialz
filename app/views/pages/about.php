d6cd8157822431e45203387785a713a2bd68fc8f
giohax <giovannifgonzaga@gmail.com>
2022-07-04 06:11:37 -0500

    Merging

---
+++ app/views/pages/about.php
@@ -0,0 +1,5 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+  <h1>About</h1>
+  <p>This is a social network app built on MVC PHP</p>
+  <p>App Version: <?php echo $data['version']; ?></p>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
